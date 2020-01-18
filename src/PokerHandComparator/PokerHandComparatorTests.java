1f55b5d5307c1693dda5a1af6a1fa68a5f2ab443
Mareike <m-glock@users.noreply.github.com>
2020-01-18 16:37:15 +0100

    added tests for same hand types

--- src/PokerHandComparator/PokerHandComparatorTests.java
+++ src/PokerHandComparator/PokerHandComparatorTests.java
@@ -1,13 +1,65 @@
 package PokerHandComparator;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class PokerHandComparatorTests {
-
+	
 	@Test
-	void test() {
+	void sortAllHandTypes() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * compare same hand types
+	 * */
+	@Test
+	void compareTwoRoyalFlushes() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoStraighFlushes() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoFourKinds() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoFullHouses() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoFlushes() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoStraights() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoThreeKinds() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoDoublePairs() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoPairs() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void compareTwoHighCards() {
 		fail("Not yet implemented");
 	}
 
