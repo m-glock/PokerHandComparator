2d28e0f13d036e008d0abb83f2d22c0ea7167d08
Mareike <m-glock@users.noreply.github.com>
2020-01-18 22:26:25 +0100

    removed unused imports

--- src/Tests/SameHandTests.java
+++ src/Tests/SameHandTests.java
@@ -2,9 +2,7 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
