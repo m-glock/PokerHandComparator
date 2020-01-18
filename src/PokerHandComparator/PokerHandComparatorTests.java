0f162788c2201ca9b469532a5fe29143d3956452
Mareike <m-glock@users.noreply.github.com>
2020-01-18 20:19:13 +0100

    added tests for flush and four of a kind

--- src/PokerHandComparator/PokerHandComparatorTests.java
+++ src/PokerHandComparator/PokerHandComparatorTests.java
@@ -2,9 +2,12 @@ package PokerHandComparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -17,6 +20,7 @@ class PokerHandComparatorTests {
 	private Hand right;
 	private Set<Card> leftCards;
 	private Set<Card> rightCards;
+	private HandComparator comp;
 	
 	@BeforeAll
     public void init() {
@@ -25,12 +29,23 @@ class PokerHandComparatorTests {
 		
 		leftCards = new HashSet<Card>();
 		rightCards = new HashSet<Card>();
+		
+		comp = new HandComparator();
     }
 	
-	@Test
+	@AfterEach
+	public void prepare() {
+		left.setCards(null);
+		right.setCards(null);
+		
+		leftCards.clear();
+		rightCards.clear();
+    }
+	
+	/*@Test
 	void sortAllHandTypes() {
 		fail("Not yet implemented");
-	}
+	}*/
 	
 	/**
 	 * compare same hand types
@@ -55,32 +70,85 @@ class PokerHandComparatorTests {
 		left.setCards(leftCards);
 		right.setCards(rightCards);
 		
-		HandComparator comp = new HandComparator();
 		int result = comp.compare(left, right);
 		System.out.println("result1 in test: " + result);
 		assertEquals(-1, result);
-		
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		result = comp.compare(right, left);
-		System.out.println("result2 in test: " + result);
-		assertEquals(1, result);
-		
-		//fail("Not yet implemented");
 	}
 	
-	/*@Test
-	void compareTwoStraighFlushes() {
-		fail("Not yet implemented");
+	@Test
+	void compareTwoStraighFlushesSameStraight() {
+		Suit suitLeft = Suit.SPADES;
+		Suit suitRight = Suit.HEARTS;
+
+		leftCards.add(new Card(suitLeft, Rank.NINE));
+		leftCards.add(new Card(suitLeft, Rank.KING));
+		leftCards.add(new Card(suitLeft, Rank.QUEEN));
+		leftCards.add(new Card(suitLeft, Rank.JACK));
+		leftCards.add(new Card(suitLeft, Rank.TEN));
+		
+		rightCards.add(new Card(suitRight, Rank.NINE));
+		rightCards.add(new Card(suitRight, Rank.KING));
+		rightCards.add(new Card(suitRight, Rank.QUEEN));
+		rightCards.add(new Card(suitRight, Rank.JACK));
+		rightCards.add(new Card(suitRight, Rank.TEN));
+		
+		left.setCards(leftCards);
+		right.setCards(rightCards);
+		
+		int result = comp.compare(left, right);
+		System.out.println("result1 in test: " + result);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	void compareTwoStraighFlushesDiffStraight() {
+		Suit suitLeft = Suit.SPADES;
+		Suit suitRight = Suit.CLUBS;
+
+		leftCards.add(new Card(suitLeft, Rank.NINE));
+		leftCards.add(new Card(suitLeft, Rank.KING));
+		leftCards.add(new Card(suitLeft, Rank.QUEEN));
+		leftCards.add(new Card(suitLeft, Rank.JACK));
+		leftCards.add(new Card(suitLeft, Rank.TEN));
+		
+		rightCards.add(new Card(suitRight, Rank.NINE));
+		rightCards.add(new Card(suitRight, Rank.EIGHT));
+		rightCards.add(new Card(suitRight, Rank.QUEEN));
+		rightCards.add(new Card(suitRight, Rank.JACK));
+		rightCards.add(new Card(suitRight, Rank.TEN));
+		
+		left.setCards(leftCards);
+		right.setCards(rightCards);
+		
+		int result = comp.compare(left, right);
+		System.out.println("result1 in test: " + result);
+		assertEquals(1, result);
 	}
 	
 	@Test
 	void compareTwoFourKinds() {
-		fail("Not yet implemented");
+		
+		leftCards.add(new Card(Suit.CLUBS, Rank.NINE));
+		leftCards.add(new Card(Suit.HEARTS, Rank.NINE));
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.NINE));
+		leftCards.add(new Card(Suit.SPADES, Rank.NINE));
+		leftCards.add(new Card(Suit.HEARTS, Rank.QUEEN));
+		
+		rightCards.add(new Card(Suit.CLUBS, Rank.FOUR));
+		rightCards.add(new Card(Suit.HEARTS, Rank.FOUR));
+		rightCards.add(new Card(Suit.DIAMONDS, Rank.FOUR));
+		rightCards.add(new Card(Suit.SPADES, Rank.FOUR));
+		rightCards.add(new Card(Suit.SPADES, Rank.ACE));
+		
+		left.setCards(leftCards);
+		right.setCards(rightCards);
+		
+		int result = comp.compare(left, right);
+		System.out.println("result1 in test: " + result);
+		assertEquals(1, result);
 	}
 	
-	@Test
+	/*@Test
 	void compareTwoFullHouses() {
 		fail("Not yet implemented");
 	}
@@ -115,4 +183,18 @@ class PokerHandComparatorTests {
 		fail("Not yet implemented");
 	}*/
 
+	/*private void fillHands() {
+		Map<String, Hand> hands = new HashMap<>();
+		
+		Hand hand1 = new Hand();
+		Set<Card> cards1 = new HashSet<Card>();
+		cards1.add(new Card(Suit.DIAMONDS, Rank.ACE));
+		cards1.add(new Card(Suit.DIAMONDS, Rank.QUEEN));
+		cards1.add(new Card(Suit.DIAMONDS, Rank.JACK));
+		cards1.add(new Card(Suit.DIAMONDS, Rank.KING));
+		cards1.add(new Card(Suit.DIAMONDS, Rank.TEN));
+		hand1.setCards(cards1);
+	}*/
 }
+
+
