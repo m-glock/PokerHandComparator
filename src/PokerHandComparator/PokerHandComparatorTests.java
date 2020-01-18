943bc028071cbf3a92d39de36784801cf97728d2
Mareike <m-glock@users.noreply.github.com>
2020-01-18 22:10:36 +0100

    fifteen tests running

--- src/PokerHandComparator/PokerHandComparatorTests.java
+++ src/PokerHandComparator/PokerHandComparatorTests.java
@@ -353,12 +353,51 @@ class PokerHandComparatorTests {
 		assertEquals(-1, result);
 	}
 	
-	/*@Test
+	@Test
 	void compareTwoDoublePairs() {
-		fail("Not yet implemented");
+		leftCards.add(new Card(Suit.HEARTS, Rank.SEVEN));
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.SEVEN));
+		leftCards.add(new Card(Suit.SPADES, Rank.FIVE));
+		leftCards.add(new Card(Suit.HEARTS, Rank.FIVE));
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.SIX));
+		
+		rightCards.add(new Card(Suit.DIAMONDS, Rank.EIGHT));
+		rightCards.add(new Card(Suit.SPADES, Rank.EIGHT));
+		rightCards.add(new Card(Suit.CLUBS, Rank.FIVE));
+		rightCards.add(new Card(Suit.DIAMONDS, Rank.FIVE));
+		rightCards.add(new Card(Suit.SPADES, Rank.FOUR));
+		
+		left.setCards(leftCards);
+		right.setCards(rightCards);
+		
+		int result = comp.compare(left, right);
+		System.out.println("result1 in test: " + result);
+		assertEquals(-1, result);
 	}
 	
 	@Test
+	void compareTwoDoublePairsSame() {
+		leftCards.add(new Card(Suit.CLUBS, Rank.SEVEN));
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.SEVEN));
+		leftCards.add(new Card(Suit.CLUBS, Rank.EIGHT));
+		leftCards.add(new Card(Suit.HEARTS, Rank.EIGHT));
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.SIX));
+		
+		rightCards.add(new Card(Suit.HEARTS, Rank.SEVEN));
+		rightCards.add(new Card(Suit.SPADES, Rank.SEVEN));
+		rightCards.add(new Card(Suit.SPADES, Rank.EIGHT));
+		rightCards.add(new Card(Suit.DIAMONDS, Rank.EIGHT));
+		rightCards.add(new Card(Suit.SPADES, Rank.FOUR));
+		
+		left.setCards(leftCards);
+		right.setCards(rightCards);
+		
+		int result = comp.compare(left, right);
+		System.out.println("result1 in test: " + result);
+		assertEquals(1, result);
+	}
+	
+	/*@Test
 	void compareTwoPairs() {
 		fail("Not yet implemented");
 	}
