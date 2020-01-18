bfcab403fd5aa648353a59112978fd11e8929522
Mareike <m-glock@users.noreply.github.com>
2020-01-18 22:26:37 +0100

    started structure for different hand tests

--- src/Tests/DifferentHandTests.java
+++ src/Tests/DifferentHandTests.java
@@ -2,13 +2,54 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import PokerHandComparator.Card;
+import PokerHandComparator.Hand;
+import PokerHandComparator.HandComparator;
+
+@TestInstance(Lifecycle.PER_CLASS)
 class DifferentHandTests {
 
+	private Hand left;
+	private Hand right;
+	private Set<Card> leftCards;
+	private Set<Card> rightCards;
+	private HandComparator comp;
+	
+	@BeforeAll
+    public void init() {
+		left = new Hand();
+		right = new Hand();
+		
+		leftCards = new HashSet<Card>();
+		rightCards = new HashSet<Card>();
+		
+		comp = new HandComparator();
+    }
+	
+	@AfterEach
+	public void prepare() {
+		left.setCards(null);
+		right.setCards(null);
+		
+		leftCards.clear();
+		rightCards.clear();
+    }
+	
+	/**
+	 * compare different hand types
+	 * */
 	@Test
 	void test() {
 		fail("Not yet implemented");
 	}
-
+	
 }
