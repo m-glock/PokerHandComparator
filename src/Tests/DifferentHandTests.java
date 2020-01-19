854d9e6fff129c233da31486d1dc01d44ca6be96
Mareike <m-glock@users.noreply.github.com>
2020-01-19 14:11:49 +0100

    added first test for one pair vs two pairs

--- src/Tests/DifferentHandTests.java
+++ src/Tests/DifferentHandTests.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.TestInstance.Lifecycle;
 import PokerHandComparator.Card;
 import PokerHandComparator.Hand;
 import PokerHandComparator.HandComparator;
+import PokerHandComparator.Rank;
+import PokerHandComparator.Suit;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class DifferentHandTests {
@@ -48,8 +50,59 @@ class DifferentHandTests {
 	 * compare different hand types
 	 * */
 	@Test
-	void test() {
+	void onePairVsTwoPairs() {
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.FIVE));
+		leftCards.add(new Card(Suit.SPADES, Rank.FIVE));
+		leftCards.add(new Card(Suit.DIAMONDS, Rank.FOUR));
+		leftCards.add(new Card(Suit.CLUBS, Rank.FOUR));
+		leftCards.add(new Card(Suit.HEARTS, Rank.ACE));
+		
+		rightCards.add(new Card(Suit.CLUBS, Rank.EIGHT));
+		rightCards.add(new Card(Suit.DIAMONDS, Rank.EIGHT));
+		rightCards.add(new Card(Suit.SPADES, Rank.ACE));
+		rightCards.add(new Card(Suit.SPADES, Rank.FOUR));
+		rightCards.add(new Card(Suit.DIAMONDS, Rank.TEN));
+		
+		left.setCards(leftCards);
+		right.setCards(rightCards);
+		
+		int result = comp.compare(left, right);
+		System.out.println("result1 in test: " + result);
+		assertEquals(1, result);
+	}
+	
+	@Test
+	void royalFlushVsStraighFlush() {
 		fail("Not yet implemented");
 	}
 	
+	@Test
+	void fullHouseVsStraight() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void StraightVsFlush() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void StraightFlushVsStraight() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void straightFlushVsFlush() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void threeOfAKindVsFullHouse() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void pairVsFullHouse() {
+		fail("Not yet implemented");
+	}
 }
